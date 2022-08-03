5e4baaa536064f0553e60310e8e695c11419343f
jeffyun123 <jeffyun.greensociety@gmail.com>
2022-08-03 11:11:56 -0700

    add custom poppins font

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App font-[Poppins]">
       <header className="App-header">
         <h1 className="text-6xl font-bold underline">Hello World</h1>
+        <p>
+          Velit ut occaecat irure laboris proident aliquip pariatur amet elit
+          qui commodo exercitation elit adipisicing. Consequat minim mollit
+          proident ea. Sint voluptate non est exercitation. Nisi quis quis sunt
+          dolore anim nisi irure quis elit pariatur anim minim ad.
+        </p>
       </header>
     </div>
   );
