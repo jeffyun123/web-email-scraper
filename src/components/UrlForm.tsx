74b2f83032424beb97e5cff0676de55c52140b05
jeffyun123 <jeffyun.greensociety@gmail.com>
2022-08-03 12:25:40 -0700

    add state for handling url input

--- src/components/UrlForm.tsx
+++ src/components/UrlForm.tsx
@@ -3,14 +3,31 @@ import { useState } from 'react';
 function UrlForm() {
 	const [url, setUrl] = useState('');
 
+	const handleOnClick = (
+		e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		e.preventDefault();
+		console.log(url);
+	};
+
+	const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUrl(e.target.value);
+	};
+
 	return (
 		<div className="h-28 flex flex-col justify-center items-center m-auto space-y-3">
 			<h2 className="text-xl">Enter a URL</h2>
 			<input
+				value={url}
+				onChange={(e) => handleOnChange(e)}
+				name="urlInput"
 				className="border-[1px] font-normal border-neutral-100 py-1 px-2 rounded-xl text-center drop-shadow-md"
 				placeholder="URL..."
 			/>
-			<button className="bg-[#046739] w-24 py-1.5 text-white rounded-lg drop-shadow-md hover:bg-[#153f2b] transition">
+			<button
+				onClick={(e) => handleOnClick(e)}
+				className="bg-[#046739] w-24 py-1.5 text-white rounded-lg drop-shadow-md hover:bg-[#153f2b] transition"
+			>
 				Search
 			</button>
 		</div>
