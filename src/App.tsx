f760e43f085cef29847a6c2b7218f37491e2d69d
jeffyun123 <jeffyun.greensociety@gmail.com>
2022-08-03 12:01:48 -0700

    create general layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import './App.css';
+import Output from './components/Output';
 
 function App() {
   return (
-    <div className="App font-[Poppins]">
-      <header className="App-header">
-        <h1 className="text-6xl font-bold underline">Hello World</h1>
-        <p>
-          Velit ut occaecat irure laboris proident aliquip pariatur amet elit
-          qui commodo exercitation elit adipisicing. Consequat minim mollit
-          proident ea. Sint voluptate non est exercitation. Nisi quis quis sunt
-          dolore anim nisi irure quis elit pariatur anim minim ad.
-        </p>
+    <div className="font-[Poppins] min-h-screen text-white flex flex-col ">
+      <header className="drop-shadow-lg bg-[#222222] py-6 pl-2">
+        <h1 className="text-2xl font-bold ">Email Web Scraper</h1>
       </header>
+      <main className="text-neutral-800 font-bold my-12">
+        <div className="h-28 flex flex-col justify-center items-center m-auto space-y-3">
+          <h2 className="text-xl">Enter a URL</h2>
+          <input
+            className="border-[1px] font-normal border-neutral-100 py-1 px-2 rounded-xl text-center drop-shadow-md"
+            placeholder="URL..."
+          />
+          <button className="bg-[#046739] w-24 py-1.5 text-white rounded-lg drop-shadow-md hover:bg-[#153f2b] transition">
+            Search
+          </button>
+        </div>
+        <Output />
+      </main>
+      <footer className="shadow-lg bg-[#222222] text-xs py-6 pl-2 fixed bottom-0 w-full">
+        Made by jeffyun123
+      </footer>
     </div>
   );
 }
---
+++ src/components/Output.tsx
@@ -0,0 +1,29 @@
+function Output() {
+  return (
+    <div className="bg-red-200 mt-10 px-4">
+      Minim elit reprehenderit eu sint magna dolore nisi quis laboris.
+      Adipisicing anim deserunt aliqua sint labore officia mollit commodo
+      nostrud esse tempor voluptate incididunt dolor. Amet duis irure ea
+      exercitation magna dolore quis. Est ut est velit ad velit minim aliquip do
+      qui do. Do magna cillum elit sint. Occaecat ullamco cupidatat magna anim
+      Lorem et sit veniam nisi officia non ex laborum proident. Anim nulla est
+      sunt tempor labore. Adipisicing ex reprehenderit excepteur nulla eiusmod
+      laborum id est voluptate Lorem aliqua ipsum. Mollit Lorem ex sunt
+      incididunt fugiat quis ut officia cupidatat reprehenderit. Pariatur ut
+      velit nostrud nisi laborum aliquip tempor mollit. Aliqua ex occaecat non
+      commodo amet et exercitation elit. Aute id enim labore cupidatat tempor
+      pariatur excepteur excepteur esse incididunt. Id nisi magna ut excepteur
+      cupidatat duis aute ex nostrud labore do pariatur. Laboris et irure sint
+      eu laborum dolor laboris sunt veniam commodo sit fugiat veniam
+      adipisicing. Duis deserunt mollit aliquip consequat do commodo qui. Anim
+      qui consequat magna duis aliquip enim qui voluptate fugiat tempor. Eu
+      mollit ut velit proident voluptate ullamco consequat fugiat adipisicing
+      mollit pariatur commodo adipisicing duis. Ut enim labore amet minim.
+      Labore qui velit eu adipisicing eiusmod tempor. Amet aute aliqua sit id
+      dolore. Minim esse elit ex irure laboris enim non et. Laboris nostrud eu
+      est laboris laboris.
+    </div>
+  );
+}
+
+export default Output;
